0f31eefa27e5cd2e185a5f352c695b199deb731d
Mohit Patel <m265pate@uwaterloo.ca>
2024-04-01 13:51:49 -0400

    space

--- commands.sh
+++ commands.sh
@@ -9,7 +9,7 @@ exiftool "-DateTimeOriginal<FileModifyDate" file
 exiftool '-createdate<datetimeoriginal' -r -if '(not $createdate and $datetimeoriginal)' .
 # Then, use CreateDate to rename the current directory files according to the format of all time (same as DropBox's magical renaming)
 exiftool -d '%Y-%m-%d %H.%M.%S%%-c.%%e' '-filename<CreateDate' .
-# Remove original files if you don't want to go back -yolo
+# Remove original files if you don't want to go back - yolo
 rm *original
 
 # mdadm - create a RAID 1 array from /dev/sda1 and /dev/sda2 (requires partitions to be created first)
